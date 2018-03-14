24d2d7dea9b9a3091fdf61cca18e32de22b4b0b0
Danielalab <olabolacrayola@hotmail.com>
2018-03-14 09:27:24 -0500

    añadiendo imagen

--- src/components/SectionMe.js
+++ src/components/SectionMe.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import Hobbies from './Hobbies';
 
-const hobbies = ['leer', 'codear', 'dormir', 'ir al cine'].map( (hobbie,index) => {
-  return <Hobbies hobbie={hobbie} key={index}/>
+const hobbies = ['leer', 'codear', 'dormir', 'ir al cine'].map( (hobbie, index) => {
+  return <Hobbies hobbie={hobbie}/>
 });
 
-const SectionMe = () => (
-  <section>
-    "Hola, soy Daniela, Front End Developer. Descubrí el mundo 'tech' en mi último año de escuela 
-    y hoy en día aspiro a desarrollarme como Full Stack Developer e involucrarme en un futuro, en 
-    el desarrollo de IA y robótica para mejorar la calidad de vida de las personas."
+const SectionMe = ({parrafo, img}) => {
+   return (<section>
+    {parrafo}
    <br/>
+   <img src={img}/>
    <h4>Hobbies</h4>
    <ul>
     {hobbies}
    </ul>
-  </section>
-)
+  </section>)
+}
+
 
 export default SectionMe
--- src/components/app.js
+++ src/components/app.js
@@ -2,10 +2,15 @@ import React from 'react';
 import Header from './Header';
 import SectionMe from './SectionMe';
 
+const data = {
+  parrafo: "Hola, soy Daniela, Front End Developer. Descubrí el mundo tech en mi último año de escuela y hoy en día aspiro a desarrollarme como Full Stack Developer e involucrarme en un futuro, en el desarrollo de IA y robótica para mejorar la calidad de vida de las personas.",
+  img: '1.jpg',
+};
+
 const App = () => (
   <div>
     <Header/>
-    <SectionMe/>
+    <SectionMe parrafo={data.parrafo} img={data.img}/>
   </div>
 )
 
